844416da68c14686da7f205f7bf7688f06e47e76
Pedro Carara <cararax@gmail.com>
2022-11-19 10:01:36 -0300

    feat(customer): adicionado repositorio e configurado tabela de customer, service salva customer no banco de dados

--- customer/src/main/java/com/carara/customer/service/CustomerService.java
+++ customer/src/main/java/com/carara/customer/service/CustomerService.java
@@ -2,10 +2,11 @@ package com.carara.customer.service;
 
 import com.carara.customer.model.Customer;
 import com.carara.customer.model.request.CustomerRegistrationRequest;
+import com.carara.customer.repository.CustomerRepository;
 import org.springframework.stereotype.Service;
 
 @Service
-public record CustomerService() {
+public record CustomerService(CustomerRepository customerRepository) {
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
                 .firstName(request.firstName())
@@ -15,7 +16,7 @@ public record CustomerService() {
         //todo: check if email is valid
         //todo: check if email is not taken
         //todo: store customer in db
-
+        customerRepository.save(customer);
 
     }
 }

3c1f8fa3f521fda06a4ff1fd3931bec3f3e2e96c
Pedro Carara <cararax@gmail.com>
2022-11-19 08:42:37 -0300

    Criado projeto de Customer, configurado porta e banner

---
+++ customer/src/test/java/com/carara/customer/CustomerApplicationTests.java
@@ -0,0 +1,21 @@
+package com.carara.customer;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class CustomerApplicationTests
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}

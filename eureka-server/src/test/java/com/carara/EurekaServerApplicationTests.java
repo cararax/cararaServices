3ad70bb24bba9f18dfebc9b0e20dcee6a70eda52
Pedro Carara <cararax@gmail.com>
2022-11-19 15:44:44 -0300

    feat(eurekaserver): adicionado projeto eurekaserver para service discovery

---
+++ eureka-server/src/test/java/com/carara/EurekaServerApplicationTests.java
@@ -0,0 +1,20 @@
+package com.carara;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class EurekaServerApplicationTests
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}

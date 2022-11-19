6946d5f56711cfc44a99de2c2e862827528554b9
Pedro Carara <cararax@gmail.com>
2022-11-19 10:32:33 -0300

    feat(fraud): adicionado projeto fraud, adicionado dependencia spring web e configado porta 8081

---
+++ fraud/src/test/java/com/carara/fraud/FraudApplicationTests.java
@@ -0,0 +1,18 @@
+package com.carara.fraud;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class FraudApplicationTests {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}

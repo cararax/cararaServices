7b97e57a120869c942625ad1f2e91b648ee00b29
Pedro Carara <cararax@gmail.com>
2022-11-20 19:09:25 -0300

    feat(notification): notification configurado para consumir mensagem da fila de notificacao

---
+++ notification/src/main/java/com/carara/notification/rabbitmq/NotificationConsumer.java
@@ -0,0 +1,24 @@
+package com.carara.notification.rabbitmq;
+
+import com.carara.clients.notification.request.NotificationRequest;
+import com.carara.notification.service.NotificationService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@AllArgsConstructor
+public class NotificationConsumer {
+
+    private final NotificationService notificationService;
+
+    @RabbitListener(queues = "${rabbitmq.queue.notification}")
+    public void consumer(NotificationRequest notificationRequest) {
+        log.info("Consumed {} from queue", notificationRequest);
+        notificationService.sendNotification(notificationRequest);
+    }
+
+}
+

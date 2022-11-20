dc7c7a845e1993ddcfaf871fc139b7ebc076fb62
Pedro Carara <cararax@gmail.com>
2022-11-20 18:42:37 -0300

    feat(amqp): adicionado produtor de mensagens

--- notification/src/main/java/com/carara/notification/NotificationApplication.java
+++ notification/src/main/java/com/carara/notification/NotificationApplication.java
@@ -1,12 +1,21 @@
 package com.carara.notification;
 
+import com.carara.amqp.RabbitMQMessageProducer;
+import com.carara.notification.config.NotificationConfig;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @EnableEurekaClient
-@SpringBootApplication
+@SpringBootApplication(
+        scanBasePackages = {
+                "com.carara.notification",
+                "com.carara.amqp"
+        }
+)
 public class NotificationApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class, args);

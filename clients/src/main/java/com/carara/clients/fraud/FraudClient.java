9c7e7512809187af3719c6e9dca5916c2ad8b898
Pedro Carara <cararax@gmail.com>
2022-11-19 21:13:37 -0300

    feat(client): adicionado interface de cliente

--- clients/src/main/java/com/carara/clients/fraud/FraudClient.java
+++ clients/src/main/java/com/carara/clients/fraud/FraudClient.java
@@ -2,13 +2,15 @@ package com.carara.clients.fraud;
 
 import com.carara.clients.fraud.response.FraudCheckResponse;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient("fraud")
+@ComponentScan
 public interface FraudClient {
 
     @GetMapping("api/v1/fraud-check/{customerId}")
-    FraudCheckResponse isFraduster(@PathVariable("customerId") Integer customerId);
+    FraudCheckResponse isFraudster(@PathVariable("customerId") Integer customerId);
 
 }
